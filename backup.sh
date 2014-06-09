216321714acc441196673377be6044ab95699808
maxdeviant <mad.mod.max@gmail.com>
2014-06-09 08:22:57 -0400

    Fix path in backup script.

--- backup.sh
+++ backup.sh
@@ -8,4 +8,4 @@ sleep 10
 tar -czf /etc/minecraft/backups/survival/$(date +%F_%H-%M).tar.gz *
 screen -p 0 -S minecraft -X eval 'stuff "say Backup complete.\015"'
 screen -p 0 -S minecraft -X eval 'stuff "save-on\015"'
-find /etc/minecraft/survival* -mtime +2 -exec rm {} -fv \;
+find /etc/minecraft/backups/survival* -mtime +1 -exec rm {} -fv \;
